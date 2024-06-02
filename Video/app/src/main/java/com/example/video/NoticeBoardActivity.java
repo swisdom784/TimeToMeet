a038f07678df9d0d63396db9c57be08ba6b29f28
yeoby97 <hydudduq@naver.com>
2024-06-02 15:21:58 +0900

    채팅

--- Video/app/src/main/java/com/example/video/NoticeBoardActivity.java
+++ Video/app/src/main/java/com/example/video/NoticeBoardActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,13 +31,17 @@ public class NoticeBoardActivity extends AppCompatActivity {
     String user;
     int roomNum;
     ListView memoListView;
-    List<message> messageList = new ArrayList<>();
     ArrayList<NoticeBoardListElement> elementList = new ArrayList<>();
+
+    EditText memoEdit;
 
     //DB 구현용 변수
     ValueEventListener valueEventListener;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    Button MakeMemoBtn;
+
+    List<HashMap<String,String>> messageList;
 
     //???? 변수
 
@@ -49,40 +56,57 @@ public class NoticeBoardActivity extends AppCompatActivity {
         int room_num = i.getIntExtra("room_num",0);
         roomNum = room_num;
         memoListView = findViewById(R.id.memoListView);
+        memoEdit = findViewById(R.id.memoEdit);
+        MakeMemoBtn = findViewById(R.id.MakeMemoBtn);
 
-
-        databaseReference.child("room").child(String.valueOf(room_num)).addListenerForSingleValueEvent(new ValueEventListener() {
+        MakeMemoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = memoEdit.getText().toString();
+                if(s.length() == 0) return;
+                memoEdit.setText("");
+                HashMap<String, String> chat =new HashMap<>();
+                chat.put("comment",s);
+                chat.put("writer",user);
+                messageList.add(chat);
+                databaseReference.child("room").child(String.valueOf(room_num)).child("messageList").setValue(messageList);
+            }
+        });
+        databaseReference.child("room").child(String.valueOf(room_num)).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //TODO : 현재는 최초 생성시만 메모 들어감, 메모를 편집해서 업로드할 시 바로 반영되게 할 것
                 room r = snapshot.getValue(room.class);
                 room = r;
                 messageList = r.getMessageList();
-
+                elementList = new ArrayList<>();
                 //TODO : FOR DEBUG
-                message temp_msg = new message("sample comment","writer");
-                messageList.add(temp_msg);
-                messageList.add(temp_msg);
+//                message temp_msg = new message("sample comment","writer");
+//                HashMap<String,String> s = new HashMap<>();
+//                s.put("comment","sample comment");
+//                s.put("writer","sample writer");
+//                messageList.add(s);
+//                messageList.add(s);
                 //TODO : FOR DEBUG
 
-                for(message msg : messageList){
-                    NoticeBoardListElement nble = new NoticeBoardListElement(msg.getComment(), msg.getWriter());
+                for(HashMap<String,String> msg : messageList){
+                    NoticeBoardListElement nble = new NoticeBoardListElement(msg.get("comment"), msg.get("writer"));
                     elementList.add(nble);
                 }//room 의 메세지를 elementlist로 옮김
                 NoticeBoardListAdapter NBLAdapter = new NoticeBoardListAdapter(getApplicationContext(),elementList);
                 memoListView.setAdapter(NBLAdapter);
                 //어댑터 장착
-                memoListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-                    public void onItemClick(AdapterView parent, View v, int position, long id){
-                        //TODO : 각 메모 클릭 시 이벤트
-                    }
-                });
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
 
+        memoListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                //TODO : 각 메모 클릭 시 이벤트
+            }
+        });
 
     }//onCreate
 }
