88266c8fd16922d86707c541b42ea89ed6d27306
ysh020702 <ysh020702@gmail.com>
2024-06-05 17:53:21 +0900

    timedatepicker 수정, 메니페스트 변경, drawble테마 추가

---
+++ Video/app/src/main/java/com/example/video/DatePickerListener.java
@@ -0,0 +1,5 @@
+package com.example.video;
+
+public interface DatePickerListener {
+    void onDateSelected(int year, int month, int day,int flag);
+}
---
+++ Video/app/src/main/java/com/example/video/TimePickerListener.java
@@ -0,0 +1,5 @@
+package com.example.video;
+
+public interface TimePickerListener {
+    void onTimeSelected(int hour, int min, int flag);
+}
